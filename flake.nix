ad56e9cdc0dfe5030fc98170d1183c43777fd24a
wucke13 <wucke13+github@gmail.com>
2026-06-15 21:36:45 +0200

    chore: update to NixOS 26.05
    The new release is out!
    
    Signed-off-by: wucke13 <wucke13+github@gmail.com>

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
 
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     flake-utils.url = "github:numtide/flake-utils";
     treefmt-nix.url = "github:numtide/treefmt-nix";
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
